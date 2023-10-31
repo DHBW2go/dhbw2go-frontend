3525cc15620180678846c927670d7d9da17f4ca3
jolinaheesen <jolina.heesen@gmail.com>
2023-10-31 09:36:58 +0100

    created project structure and first screen with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import {Image, View} from 'react-native-ui-lib';
+import {View} from 'react-native-ui-lib';
 import {Component} from 'react';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+import {faSquareCheck} from '@fortawesome/free-solid-svg-icons/faSquareCheck';
+import {faMugSaucer} from '@fortawesome/free-solid-svg-icons';
+import HomeScreen from './screens/HomeScreen';
+
+library.add(fab, faSquareCheck, faMugSaucer);
 
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Image source={require('../assets/logo.png')} />
+        <HomeScreen />
       </View>
     );
   }
---
+++ src/ui/Style.tsx
@@ -0,0 +1,16 @@
+import {Typography, Colors, Spacings} from 'react-native-ui-lib';
+
+Colors.loadColors({
+  white: '#FFFFFF',
+  red: '#E30813',
+  lightgrey: '#D9D9D9',
+  grey: '#A6A6A6',
+  darkgrey: '#545454',
+});
+
+Typography.loadTypographies({
+  h1: {fontSize: 58, fontWeight: '300', lineHeight: 80},
+  h2: {fontSize: 46, fontWeight: '300', lineHeight: 64},
+});
+
+Spacings.loadSpacings({});
