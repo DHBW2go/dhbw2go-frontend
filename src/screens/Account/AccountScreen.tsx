481d188144398e8c90ad94e902807a1e14f5516f
jolinaheesen <jolina.heesen@gmail.com>
2024-03-03 15:59:15 +0100

    Added Account Screen with mocked data

---
+++ src/screens/Account/AccountScreen.tsx
@@ -0,0 +1,85 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Alert} from 'react-native';
+import {View, Text, Image, Button} from 'react-native-ui-lib';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
+import ChangeModal from './ChangeModal';
+
+const style = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 22,
+    marginBottom: 10,
+  },
+  button: {
+    fontSize: 22,
+    marginBottom: 10,
+    ButtonSize: 'large',
+  },
+});
+
+const mockBackendData = {
+  username: 'John Doe',
+  email: 'johndoe@example.com',
+  profileImage:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF_BtB3w_jb8m-8SkMsQ1dbA7L7-ZqDrWC8llSddRKwA&s',
+};
+
+function AccountScreen() {
+  const [userData, setUserData] = useState(mockBackendData);
+  const [isChangePasswordModalVisible, setIsChangePasswordModalVisible] =
+    useState(false);
+
+  useEffect(() => {
+    // Simulating backend call
+    setTimeout(() => {
+      setUserData(mockBackendData);
+    }, 1000);
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleChangePassword = (newPassword: string) => {
+    // Logic to change password
+    setIsChangePasswordModalVisible(false);
+  };
+
+  return (
+    <View style={style.view}>
+      {userData && userData.profileImage ? (
+        <Image source={{uri: userData.profileImage}} style={style.image} />
+      ) : (
+        <FontAwesomeIcon icon={faUser} size={100} />
+      )}
+      <Text style={style.label}>Benutzername: {userData.username}</Text>
+      <Text style={style.label}>E-Mail: {userData.email}</Text>
+      <Button
+        label={'Passwort ändern'}
+        backgroundColor={'#A6A6A6'}
+        style={style.button}
+        onPress={() => setIsChangePasswordModalVisible(true)}
+      />
+      <Button
+        label={'Abmelden'}
+        backgroundColor={'#E30813'}
+        onPress={() => Alert.alert('Abmelden!')}
+      />
+      <ChangeModal
+        isVisible={isChangePasswordModalVisible}
+        onClose={() => setIsChangePasswordModalVisible(false)}
+        onChangePassword={handleChangePassword}
+      />
+    </View>
+  );
+}
+
+export default AccountScreen;
