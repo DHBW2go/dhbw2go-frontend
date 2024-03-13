4b6d298198cf4bc5f213dd018554ee6698ed3ddd
jolinaheesen <jolina.heesen@gmail.com>
2024-03-13 18:53:16 +0100

    changed header and initial route

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,93 @@ import AccountScreen from './screens/Account/AccountScreen';
 import ToDoScreen from './screens/ToDo/ToDoScreen';
 import GradesScreen from './screens/GradesScreen';
 import React from 'react';
+import LoginScreen from './screens/LoginScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import CourseScreen from './screens/CourseScreen';
 
 library.add(faBars, faUser);
 
 const Stack = createNativeStackNavigator();
+
+const HeaderRight = () => <AccountShortcut />;
+const HeaderLeft = () => <BurgerMenu />;
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="WelcomeScreen"
         screenOptions={{
-          headerTitle: 'DHBW2go',
           headerTitleStyle: {
             fontWeight: 'bold',
             color: '#E30813',
           },
-          headerRight: () => <AccountShortcut />,
-          headerLeft: () => <BurgerMenu />,
         }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="CalendarScreen" component={CalendarScreen} />
-        <Stack.Screen name="GradesScreen" component={GradesScreen} />
-        <Stack.Screen name="AccountScreen" component={AccountScreen} />
-        <Stack.Screen name="ToDoScreen" component={ToDoScreen} />
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={() => ({headerTitle: 'Willkommen'})}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={() => ({headerTitle: 'Login'})}
+        />
+        <Stack.Screen
+          name="RegistrationScreen"
+          component={RegistrationScreen}
+          options={() => ({headerTitle: 'Registration'})}
+        />
+        <Stack.Screen
+          name="CourseScreen"
+          component={CourseScreen}
+          options={() => ({headerTitle: 'Kursauswahl'})}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            headerTitle: 'DHBW2go',
+            headerRight: HeaderRight,
+            headerLeft: HeaderLeft,
+          }}
+        />
+        <Stack.Screen
+          name="CalendarScreen"
+          component={CalendarScreen}
+          options={{
+            headerTitle: 'Kalender',
+            headerRight: HeaderRight,
+            headerLeft: HeaderLeft,
+          }}
+        />
+        <Stack.Screen
+          name="GradesScreen"
+          component={GradesScreen}
+          options={{
+            headerTitle: 'Noten',
+            headerRight: HeaderRight,
+            headerLeft: HeaderLeft,
+          }}
+        />
+        <Stack.Screen
+          name="AccountScreen"
+          component={AccountScreen}
+          options={{
+            headerTitle: 'Account',
+            headerRight: HeaderRight,
+            headerLeft: HeaderLeft,
+          }}
+        />
+        <Stack.Screen
+          name="ToDoScreen"
+          component={ToDoScreen}
+          options={{
+            headerTitle: 'To-Do Liste',
+            headerRight: HeaderRight,
+            headerLeft: HeaderLeft,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
